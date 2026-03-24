271c4a01414fd70027b21fb9ef745bac777caab1
MolodzeffDanila <russgun1@mail.ru>
2026-03-25 01:23:43 +0300

    feat: added rollup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default [{
+    input: "src/index.ts",
+    output: [{
+            file: "dist/index.js",
+            format: "esm"
+        },
+        {
+            file: "dist/index.cjs",
+            format: "cjs"
+        }
+    ],
+    plugins: [typescript()],
+}];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-const classnames = () => ({})
+import { ClassnamesObjType, ClassnamesType } from "./types";
+
+//const buildStrFromClassObject = (obj: ClassnamesObjType) => obj.reduce()
+
+export const classnames : ClassnamesType = (...props) => {
+    let classString = '';
+
+    props.forEach((item) => {
+        if(typeof item === "string"){
+            classString += `${item} `;
+        }else if(){
+
+        }
+    })
+
+    return classString;
+}
+
+console.log(classnames("QWE", "WWDQ", "type tt", {
+    "key": true
+}))
